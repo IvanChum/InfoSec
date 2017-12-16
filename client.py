2d70dad12c93626a3fe129a3fa34dc253942b4a4
Иван Чумаков <chumakov@okbsapr.ru>
2017-12-16 10:58:03 +0300

    Стабильная версия, но обрабатывается только один блок.

--- client.py
+++ client.py
@@ -1,36 +1,68 @@
 import socket
-
+from kuznechik import kuznechik
+import binascii
+import time
+from DFH import DiffieHellman
 
 if __name__ == "__main__":
     print("Start Client")
-    try:
-        HOST, port = input("With who, you want to talk? IP Port\n").split(" ")
-    except ValueError:
-        print("You should enter ")
-    if HOST.isdigit():
-        ip = int(HOST)
-    else:
-        ip = HOST
-    # Create a socket (SOCK_STREAM means a TCP socket)
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        try:
-            # Connect to server and send data
-            sock.connect((ip, int(port)))
-            while True:
-                try:
-                    data = input("Enter your massage:\n")
-                    sock.sendall(bytes(data + "\n", "utf-8"))
-
-                    # Receive data from the server and shut down
-                    received = str(sock.recv(1024), "utf-8")
-
-                    print("Sent:     {}".format(data))
-                    print("Received: {}".format(received))
-                except BrokenPipeError:
-                    print("Close Client")
-                    sock.close()
-        except KeyboardInterrupt:
-            print("Close Client")
-            sock.close()
-        except TypeError:
-            print("Error with host address! You must enter host IP and Port.")
+    key = '8899aabbccddeeff0011223344556677fedcba98765432100123456789abcdef'
+    while True:
+        while True:
+            try:
+                HOST, port = input("With who, you want to talk? IP Port\n").split(" ")
+            except ValueError:
+                print("Error! You should enter only IP and Port")
+                continue
+            except KeyboardInterrupt:
+                print("Close Client.")
+                exit(-1)
+            try:
+                if HOST.isdigit():
+                    ip = int(HOST)
+                else:
+                    ip = HOST
+                if not port.isdigit():
+                    raise NameError
+                break
+            except NameError:
+                    print("Wrong IP or Port.")
+        # Create a socket (SOCK_STREAM means a TCP socket)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            try:
+                # Connect to server and send data
+                sock.connect((ip, int(port)))
+                while True:
+                    try:
+                        data_str = input("Enter your massage:\n")
+                        if not data_str:
+                            raise BrokenPipeError
+                        data = bytes(data_str, "utf-8")
+                        text = binascii.hexlify(data).zfill(32)
+                        crypto_text = kuznechik(key).encrypt(text)
+                        print(crypto_text)
+                        sock.send(crypto_text)
+                        # Receive data from the server and shut down
+                        crypto_received = sock.recv(2048)
+                        print(crypto_received)
+                        received_byte = kuznechik(key).decrypt(crypto_received)
+                        print(received_byte)
+                        received_hex = binascii.unhexlify(received_byte)
+                        print(received_hex)
+                        received_str = str(received_hex, "utf-8")
+                        print("Sent:     {}".format(data_str))
+                        print("Received: {}".format(received_str))
+                    except BrokenPipeError:
+                        print("Close Client")
+                        sock.close()
+                        exit(0)
+            except KeyboardInterrupt:
+                print("Close Client")
+                sock.close()
+                break
+            #except TypeError:
+            #    print("Error with host address! You must enter correct host IP and Port.")
+            except socket.gaierror:
+                print("Error with host address! Nodename nor servname provided, or not known.")
+            except ConnectionRefusedError:
+                print("Connection refused.")
--- server.py
+++ server.py
@@ -1,27 +1,58 @@
 import socketserver
+from kuznechik import kuznechik
+import binascii
+from DFH import DiffieHellman
 
 
-class MyTCPHandler(socketserver.StreamRequestHandler):
+class MyTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
+        key = '8899aabbccddeeff0011223344556677fedcba98765432100123456789abcdef'
         print("...Connecting from: ", self.client_address)
         while True:
-            self.data = self.rfile.readline().strip()
-            if not self.data:
+            data = self.request.recv(2048)
+            if not data:
                 print("Close connection.")
                 print("Waiting for new connection...")
                 break
-            print("{} wrote:".format(self.client_address[0]))
-            print(self.data.decode("utf-8"))
-            self.wfile.write(self.data.upper())
+            text_hex = kuznechik(key).decrypt(data)
+            text_str = str(binascii.unhexlify(text_hex), "utf-8")
+            print("{} wrote:   {}".format(self.client_address[0], text_str))
+            text_str = text_str.upper()
+            print("Send to {}: {}".format(self.client_address[0], text_str))
+            text_byte = bytes(text_str, "utf-8")
+            print(text_byte)
+            text_hex = binascii.hexlify(text_byte).zfill(32)
+            print(text_hex)
+            crypto_text = kuznechik(key).encrypt(text_hex)
+            print(crypto_text)
+            self.request.send(crypto_text)
 
 
 if __name__ == "__main__":
-    HOST, PORT = input("Enter address of server: IP Port\n").split(" ")
+    while True:
+        try:
+            HOST, port = input("Enter host address: IP Port\n").split(" ")
+        except ValueError:
+            print("Error! You should enter only IP and Port")
+            continue
+        except KeyboardInterrupt:
+            print("Close Server.")
+            exit(-1)
+        try:
+            if HOST.isdigit():
+                ip = int(HOST)
+            else:
+                ip = HOST
+            if not port.isdigit():
+                raise NameError
+            break
+        except NameError:
+            print("Wrong IP or Port.")
     print("Server start")
     print("Waiting for connection...")
 
     # Create the server, binding to localhost on port which user chooses
-    with socketserver.TCPServer((HOST, int(PORT)), MyTCPHandler) as server:
+    with socketserver.TCPServer((ip, int(port)), MyTCPHandler) as server:
         try:
             # Activate the server; this will keep running until you
             # interrupt the program with Ctrl-C
